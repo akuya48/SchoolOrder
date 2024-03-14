61e0eca0fc9046975f3b36365f96a698a4113bfe
余伟 <1102826184@qq.com>
2024-03-14 15:34:51 +0800

    新增员工分页查询

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -20,4 +22,11 @@ public interface EmployeeMapper {
             "values " +
             "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser},#{status})")
     void insert(Employee employee);//在yml中开启驼峰命名
+
+    /**
+     * 分页查询
+     * @param dto
+     * @return
+     */
+    Page<Employee> pageQuery(EmployeePageQueryDTO dto);
 }
